8686594d64074e126c0184e9f5f85ef7f57098fe
SebasBeltranM <142366728+SebasBeltranM@users.noreply.github.com>
2023-09-04 17:36:08 -0500

    subiendo todo

--- co.edu.uniquindio.universidad/src/main/java/co/edu/uniquindio/universidad/Docente.java
+++ co.edu.uniquindio.universidad/src/main/java/co/edu/uniquindio/universidad/Docente.java
@@ -1,4 +1,41 @@
 package co.edu.uniquindio.universidad;
 
 public class Docente {
+    private String nombre;
+    private int edad;
+    private String correo;
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre (String nombre){
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo (){
+        return correo;
+    }
+
+    public void setCorreo (String correo){
+        this.correo = correo;
+    }
+
+    public Docente (){
+    }
+    public Docente (String nombre, int edad, String correo){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.correo = correo;
+    }
+
+
 }

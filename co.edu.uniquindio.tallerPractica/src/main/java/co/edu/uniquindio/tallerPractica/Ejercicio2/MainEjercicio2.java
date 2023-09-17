496577a72efd13ae88db35bc21162af343a5d75f
SebasBeltranM <142366728+SebasBeltranM@users.noreply.github.com>
2023-09-17 18:21:36 -0500

    ultima actualizacion

---
+++ co.edu.uniquindio.tallerPractica/src/main/java/co/edu/uniquindio/tallerPractica/Ejercicio2/MainEjercicio2.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.tallerPractica.Ejercicio2;
+
+public class MainEjercicio2 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ co.edu.uniquindio.tallerPractica/src/main/java/co/edu/uniquindio/tallerPractica/Ejercicio3/MainEjercicio3.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.tallerPractica.Ejercicio3;
+
+import com.sun.tools.javac.Main;
+
+public class MainEjercicio3 {
+    public static void main(String[] args) {
+
+    }
+}
